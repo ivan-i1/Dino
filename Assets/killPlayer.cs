d7192f81071a1e295b7864d14a6b428924ddad5d
ivan-i1 <ivan_i1@hotmail.com>
2014-06-18 11:03:26 -0500

    Kill box First Attempt

--- Assets/killPlayer.cs
+++ Assets/killPlayer.cs
@@ -3,17 +3,28 @@ using System.Collections;
 
 public class killPlayer : MonoBehaviour {
 
-	public GameObject target;
-
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("Start!");
+		Debug.Log ("Trigger: " + BoxCollider2D.isTrigger);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if(target.collider.gameObject){
-			Destroy(target);
-		}
+
 	}
+
+	/*void OnTriggerEnter2D(Collider2D target){
+			Destroy(gameObject);
+	}*/
+
+	/*void OnCollision(){
+			Destroy(gameObject);
+	}*/
+
+
+	void OnTriggerEnterD2(Collider2D player){
+		Debug.Log ("Trigger2D");
+		Destroy (player.gameObject);
+		}
 }
