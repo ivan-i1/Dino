f2d65f006f72a00b4756e0bae68827b6c7fb272e
Mauricio Zaragoza <kingk912@gmail.com>
2014-06-17 22:14:11 -0700

    Added camera resizing

---
+++ Assets/ResizeCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResizeCamera : MonoBehaviour {
+
+	void Awake(){
+		camera.orthographicSize = ((Screen.height/2.0f)/100f);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
