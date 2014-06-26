93e904376a7fb08be273e886aadd167da5d14015
Mauricio Zaragoza <kingk912@gmail.com>
2014-06-25 23:41:00 -0700

    Added pickup glow
    Created spike hazards

---
+++ Assets/Standard Assets/Scripts/DamageTile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DamageTile : MonoBehaviour
+{
+    public float Damage = 0.05f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.tag)
+        {
+            case "Player":
+                other.GetComponentInChildren<PlayerEnergy>().energy -= Damage;
+                break;
+
+            case "Enemy":
+                Destroy(other.transform.parent.gameObject);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
