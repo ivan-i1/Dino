123a3ea4c90d7a6a4d7f84b4c0c10f17f3d9ecb2
Mauricio Zaragoza <kingk912@gmail.com>
2014-06-26 09:35:47 -0700

    Added lowerbound prefab

---
+++ Assets/Standard Assets/Scripts/LowerBounds.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LowerBounds : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().Hurt(1f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Standard Assets/Scripts/ResizeCamera.cs
+++ Assets/Standard Assets/Scripts/ResizeCamera.cs
@@ -6,7 +6,7 @@ public class ResizeCamera : MonoBehaviour
     void Awake()
     {
         //camera.orthographicSize = ((Screen.height / 2.0f) / 100f);
-        Screen.SetResolution(640, 280, false, 60);
+        Screen.SetResolution(640, 350, false, 60);
     }
 
     // Use this for initialization
